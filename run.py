dc592526099045425b2ab20e33f83f42bd620f27
Martin Schrimpf <m4rtinsch@gmail.com>
2018-11-12 14:58:26 -0500

    move CORnets into own package
    otherwise, they cannot be imported

--- run.py
+++ run.py
@@ -81,7 +81,7 @@ set_gpus(FLAGS.ngpus)
 
 
 def get_model():
-    mod = importlib.import_module(f'cornet_{FLAGS.model.lower()}')
+    mod = importlib.import_module(f'cornet.cornet_{FLAGS.model.lower()}')
     model = getattr(mod, f'CORnet_{FLAGS.model}')()
     return model
 
